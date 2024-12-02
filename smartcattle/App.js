3868cb2e5a55b2f49dfd4c7e20490a1924b26d00
B1njamin <ibakayoko2005@gmail.com>
2024-12-02 23:49:40 +0000

    Essaie de commit

--- smartcattle/App.js
+++ smartcattle/App.js
@@ -2,12 +2,14 @@ import { Text, View } from "react-native";
 import { Human } from "./components/Human/Human";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
+import { Sebe } from "./components/Sebe/Sebe";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ backgroundColor: "#2D6A4F", flex: 1 }}>
         <Text style={s.grandtitre}>Smartcattle</Text>
+        <Sebe></Sebe>
       </SafeAreaView>
     </SafeAreaProvider>
   );
---
+++ smartcattle/components/Sebe/Sebe.jsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export function Sebe(){
+    return(
+        <Text> Je suis Sebe</Text>
+    )
+}
